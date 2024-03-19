83cc88d27d658caee6220f1e4164ed5a6b3028b2
Matthew Vernick <mevernick@wpi.edu>
2024-03-18 21:44:42 -0400

    Started entry menu

--- Hands-On9/Program.cs
+++ Hands-On9/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,13 @@ namespace Hands_On9
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Which application would you like to review?\n\t1. Logger\n\t2. Animal factory");
+            string menuSelection = Console.ReadLine();
+            if (menuSelection == '1')
+            {
+                Log();
+
+            }
         }
     }
 }
